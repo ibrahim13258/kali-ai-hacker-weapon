753a0fa52fdc39bc2f6530f99c888452d09c8cf1
Mohd laikansari <mmohddaudkahan70@gmail.com>
2025-08-21 20:30:07 +0530

    Create kali_dependencies.sh

---
+++ kali_dependencies.sh
@@ -0,0 +1,7 @@
+# Update system
+sudo apt update && sudo apt upgrade -y
+
+# Install dependencies
+sudo apt install -y python3-pip python3-venv git curl wget
+sudo apt install -y vnc4server xrdp remmina
+sudo apt install -y net-tools iptables tcpdump
